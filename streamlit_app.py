00cb5e7213f8c9ba0cd828b041baf45c528ba21d
xinyang1243 <117121115+xinyang1243@users.noreply.github.com>
2022-11-06 20:40:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,8 @@
 import streamlit as st
 
-from streamlit_faker import all_commands, get_streamlit_faker
-
-if "seed" not in st.session_state:
-    st.session_state["seed"] = 12
-
-st.title("🥷 Streamlit Faker")
-
-st.header("What's that?")
-st.write(
-    "Streamlit Faker offers a way to quickly fake some Streamlit commands. Say you want to quickly fake a plausible Streamlit UI for some markdown, a selectbox and a chart, well all you need is the following piece of code:"
-)
-
-st.code(
-    """
 from streamlit_faker import get_streamlit_faker
+
+st.title("🥷 Example of Streamlit Faker")
 
 st_faker = get_streamlit_faker()
 st_faker.subheader()
@@ -22,44 +10,3 @@ st_faker.markdown()
 st_faker.selectbox()
 st_faker.slider()
 st_faker.map()
-""",
-    language="python",
-)
-
-generate = st.button("🥷 Fake it!")
-
-if generate:
-    st.session_state.seed += 1
-
-st_faker = get_streamlit_faker(seed=st.session_state.seed)
-with st.spinner("Faking..."):
-    st_faker.subheader()
-    st_faker.markdown()
-    st_faker.selectbox()
-    st_faker.slider()
-    st_faker.map()
-
-
-with st.expander("Lookup all available Streamlit Faker commands"):
-    for cmd in all_commands:
-        if cmd.startswith("_"):
-            continue
-        else:
-            st.write(f"- `st_faker.{cmd}`")
-
-    # for cmd in all_commands:
-    #     if cmd.startswith("_") or cmd in (
-    #         "camera_input",
-    #         "snow",
-    #         "balloons",
-    #         "exception",
-    #     ):
-    #         continue
-    #     try:
-    #         st.write(f"`faker.{cmd}()`")
-    #         st_faker = get_streamlit_faker(seed=st.session_state.seed).__getattr__(
-    #             cmd
-    #         )()
-    #     except:
-    #         st.text("(WIP)")
-    #     st.write("---")
